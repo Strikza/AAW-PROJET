4538da00de5fcd36f961665ab7ae09eef8a18d9a
Fasory <72564803+Fasory@users.noreply.github.com>
2022-10-25 16:56:28 +0200

    Test connection with db

--- projet_zoo-aaw/src/serveur.js
+++ projet_zoo-aaw/src/serveur.js
@@ -1,10 +1,17 @@
 require('dotenv').config()
 const express = require(express)
 const { useReducer } = require('react')
+const mysql = require('mysql')
 
 const app = express()
 const port = process.env.PORT
 const animals = [{id:1, name:'potit_robot'}, {id:2, name:'potit_deamon'}]
+const zoo_db = mysql.createPool({
+    host: process.env.HOST,
+    user: process.env.USER,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE
+})
 
 app.use("/", express.static("public"))
 app.use(express.json())
@@ -13,3 +20,17 @@ app.get("/message", (req, res)=>{
     console.log("[LOG] : Page des animaux")
     res.send("mes animaux")
 });
+
+app.get("/", (req, res)=>{
+    console.log("[LOG] : Page des animaux")
+    const insert = "INSERT INTO Test (name) VALUES ('Second Name');"
+    zoo_db.query(insert)
+});
+
+app.listen(5173, () => {
+    const insert = "SELECT * FROM Test;"
+    zoo_db.query(insert, (err, result) => {
+        console.log(result)
+    })
+    console.log("tag")
+})
