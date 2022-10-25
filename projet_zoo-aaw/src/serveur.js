1d6237097743084e6f4ab79bbba0c48ae3f7f3e7
Fasory <72564803+Fasory@users.noreply.github.com>
2022-10-25 12:04:18 +0200

    Adding environment variables

--- projet_zoo-aaw/src/serveur.js
+++ projet_zoo-aaw/src/serveur.js
@@ -1,11 +1,13 @@
+require('dotenv').config()
 const express = require(express)
 const { useReducer } = require('react')
+
 const app = express()
-const port = 3000
+const port = process.env.PORT
+const animals = [{id:1, name:'potit_robot'}, {id:2, name:'potit_deamon'}]
+
 app.use("/", express.static("public"))
 app.use(express.json())
-
-const animals = [{id:1, name:'potit_robot'}, {id:2, name:'potit_deamon'}]
 
 app.get("/message", (req, res)=>{
     console.log("[LOG] : Page des animaux")
