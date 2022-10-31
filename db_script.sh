c6943d4428a41fa1c10be7694f49fd70dd9073dc
Fasory <72564803+Fasory@users.noreply.github.com>
2022-10-31 16:47:29 +0100

    Update condition

--- db_script.sh
+++ db_script.sh
@@ -2,17 +2,32 @@
 
 export $(grep -v '^#' ./projet_zoo-aaw/.env | xargs)
 
-echo Verification des outils pour la creation de la base MySQL
-apt-get update
-apt-get install mysql-server
-
-echo Montage de la base
-service mysql start
-mysql -h ${HOST} --user="${USER}" --password="${PASSWORD}" < "initialize_db.sql"
-
 if [[ $? -eq 0 ]]
 then
-  echo La base est operationnelle
+
+  echo Verification des outils pour la creation de la base MySQL
+
+  if [[ $? -eq 0 ]]
+  then
+
+  apt-get update
+  apt-get install mysql-server
+
+    echo Montage de la base
+    service mysql start
+    mysql -h ${HOST} --user="${USER}" --password="${PASSWORD}" < "initialize_db.sql"
+
+    if [[ $? -eq 0 ]]
+    then
+      echo La base est operationnelle
+    else
+      echo Echec du montage de la base
+    fi
+
+  else
+      echo Echec lors de la verification des outils
+  fi
+
 else
-  echo Echec du montage de la base
+    echo Echec lors du chargement des variables d\'environnement
 fi
