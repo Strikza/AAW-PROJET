9ff08dba45fbf135317a419fa683885c1e9e5fb9
Samuel <61194052+Strikza@users.noreply.github.com>
2022-12-15 15:36:03 +0100

    Connect redirection is working

--- projet_zoo-aaw/src/routes/connexion/index.jsx
+++ projet_zoo-aaw/src/routes/connexion/index.jsx
@@ -1,34 +1,52 @@
 import { Link, Form } from 'react-router-dom';
 import { InputGroup } from 'react-bootstrap';
 import { tryConnect } from './connexion';
+import { useState } from "react";
 
 import '../../css/form.css'
 
 export async function action({request, param}){
-    await tryConnect({request, param});
+    return tryConnect({request, param});
 }
 
 export default function Root() {
+
+    const [user, setUser] = useState("");
+
+    const [password, setPassword] = useState("");
+
+    const login=()=>{
+        fetch("/api/connect", {
+            method:"POST",
+            body:{
+                user,
+                password
+            }
+        })
+    }
+
     return (
         <div>
-            <Form className="login" method='post'>
+            <Form 
+            className="login" 
+            method='post'
+              >
                 <h1>Connexion</h1>
                 <hr/>
                 <InputGroup>
                     <div className="column">
                         <p className="label">Nom d'utilisateur</p>
-                        <input className="text" id="pseudonyme" type={"text"}></input>
+                        <input className="text" type={"text"} value={user} onChange={(e) => setUser(e.target.value)}></input>
                         <p className="label">Mot de passe</p>
-                        <input className="text" id="password" type={"password"}></input>
+                        <input className="text" type={"password"} value={password} onChange={(e) => setPassword(e.target.value)}></input>
                     </div>
                     <button className="button" type="submit" value="Se connecter">Se connecter</button>
                 </InputGroup>
-                
                 <hr/>
+                <Link to={"/inscription"}>
+                    <button className="button">S'inscrire</button>
+                </Link>
             </Form>
-            <Link to={"/inscription"}>
-                <button className="button">S'inscrire</button>
-            </Link>
         </div>
     );
   }
--- projet_zoo-aaw/src/serveur.js
+++ projet_zoo-aaw/src/serveur.js
@@ -63,100 +63,17 @@ app.get("/api/animals/:id", (req, res, next)=>{
   })
 });
 
-app.post("/api/connexion", (req, res, next)=>{
+app.post("/api/connect", (req, res, next)=>{
   console.log("[LOG] : Page des utilisateurs")
+
+  const user = req.body
+
+  console.log(user)
   
-  res.sendStatus(220)
-  //pool.query(queryFetchAll, (err, result) => {
+  res.sendStatus(200)
+  //pool.query(queryFetchAll, (err,result) => {
   //  res.send(result.rows)
   //})
 });
 
 app.listen(port)
-
-
-// import localforage from "localforage";
-// import { matchSorter } from "match-sorter";
-// import sortBy from "sort-by";
-
-// import express from 'express';
-// const app = express();
-// import pg from "pg";
-// import dotenv from "dotenv";
-
-
-// dotenv.config();
-// console.log("connecting to", process.env.POSTGRESQL_ADDON_URI);
-// const pgClient = new pg.Client(process.env.POSTGRESQL_ADDON_URI);
-// pgClient.connect();  
-
-// export async function getAnimals(query) {
-//   await fakeNetwork("getAnimals:${query}");
-//   let animals = await localforage.getItem("animals");
-//   if (!animals) animals = [];
-//   if (query) {
-//     animals = matchSorter(animals, query, { keys: ["name"] });
-//   }
-//   return animals.sort(sortBy("name", "createdAt"));
-// }
-
-// export async function createAnimal() {
-//   await fakeNetwork();
-//   let id = Math.random().toString(36).substring(2, 9);
-//   let animal = { id: Math.random().toString(36).substring(2, 9), name: "Bourletos le Vrai" };
-//   let animals = await getAnimals();
-//   animals.unshift(animal);
-//   await set(animals);
-//   return animal;
-// }
-
-// export async function getAnimal(id) {
-//   await fakeNetwork(`animal:${id}`);
-//   let animals = await localforage.getItem("animals");
-//   let animal = animals.find(animal => animal.id === id);
-//   //return animal ?? null;
-//   return animal ?? { id: 1, name: "Bourletos le Fék" }
-// }
-
-// export async function updateAnimal(id, updates) {
-//   await fakeNetwork();
-//   let animals = await localforage.getItem("animals");
-//   let animal = animals.find(animal => animal.id === id);
-//   if (!animal) throw new Error("No animal found for", id);
-//   Object.assign(animal, updates);
-//   await set(animals);
-//   return animal;
-// }
-
-// export async function deleteAnimal(id) {
-//   let animals = await localforage.getItem("animals");
-//   let index = animals.findIndex(animal => animal.id === id);
-//   if (index > -1) {
-//     animals.splice(index, 1);
-//     await set(animals);
-//     return true;
-//   }
-//   return false;
-// }
-
-// function set(animals) {
-//   return localforage.setItem("animals", animals);
-// }
-
-// // fake a cache so we don't slow down stuff we've already seen
-// let fakeCache = {};
-
-// async function fakeNetwork(key) {
-//   if (!key) {
-//     fakeCache = {};
-//   }
-
-//   if (fakeCache[key]) {
-//     return;
-//   }
-
-//   fakeCache[key] = true;
-//   return new Promise(res => {
-//     setTimeout(res, Math.random() * 800);
-//   });
-// }
